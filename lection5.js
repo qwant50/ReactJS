dd8c98aeb5e60f359639daf62cb3d8ff2244cb14
qwant50 <qwantonline@gmail.com>
2017-06-24 13:02:38 +0300

    first commit

--- lection5.js
+++ lection5.js
@@ -1,3 +1,26 @@
 'use strict';
 
+function Sqrt(number){
+    this.number = number;
+    this.calc = function() {
+        return Math.sqrt(this.number);
+    }
+}
+
+function User(name) {
+    var _name = '';
+    function validName(name){
+        if (typeof name === 'string'){
+            _name = name;
+        }
+    }
+    validName(name);
+    this.setName = function(name){
+        validName(name);
+    };
+    this.getName = function(){
+        return _name;
+    }
+}
+
 
