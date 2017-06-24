9279683a03e2368d56434bf3e562add1ee596533
qwant50 <qwantonline@gmail.com>
2017-06-24 17:32:58 +0300

    practice5/1

---
+++ practice5.js
@@ -0,0 +1,43 @@
+'use strict';
+
+function AudioPlayer(volume) {
+    var _volume = 0;
+
+    function validVolume(volume) {
+        volume = parseInt(volume);
+        if (!isNaN(volume) && volume >= 0 && volume <= 100) {
+            _volume = volume;
+        }
+    }
+    validVolume(volume);
+    this.volume = function (volume) {
+        if (volume === undefined) {
+            return _volume;
+        } else {
+            validVolume(volume);
+        }
+    };
+}
+
+var coolPlayer = new AudioPlayer(23);
+console.log(coolPlayer.volume());
+coolPlayer.volume(true);
+console.log(coolPlayer.volume());
+coolPlayer.volume(-1);
+console.log(coolPlayer.volume());
+coolPlayer.volume(101);
+console.log(coolPlayer.volume());
+coolPlayer.volume(null);
+console.log(coolPlayer.volume());
+coolPlayer.volume(undefined);
+console.log(coolPlayer.volume());
+coolPlayer.volume(0);
+console.log(coolPlayer.volume());
+coolPlayer.volume(100);
+console.log(coolPlayer.volume());
+coolPlayer.volume('50');
+console.log(coolPlayer.volume());
+coolPlayer.volume('45tyt');
+console.log(coolPlayer.volume());
+
+
